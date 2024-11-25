053534e47fbbad019973f735b778a84c0e167069
Alessandra <tabangcuraalessandra03@gmail.com>
2024-11-25 12:55:47 +0800

    initial project

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const express = require ('express')
+const app = express()
+
+app.get('/', (req, res) => { 
+    res.send('Hello node API haha')
+
+})
+app.get('/API', (req, res) => { 
+    res.send('this is our api s')
+
+})
+app.listen(3000, ()=> {
+    console.log('Node API app is running or port 3000')
+})
